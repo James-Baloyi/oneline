68846aeca6985463f7f67205bc3a03a72ae469c9
James Baloyi <james_baloyi@yahoo.com>
2019-12-02 09:02:22 +0200

    Added XYZ references

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+constructor(){
+  super()
+  this.state = {
+    email: '',
+    surname: '',
+    name: '',
+    pass: ''
+  }
+}
+
+logEvent(event){
+  console.log(event)
+}
+componentDidMount(){
+  console.log('on')
+}
+
+render(){
+  return(
+    <center>
+    <table id='main-table'>
+    <tbody>
+      <tr>
+          <td id='left'>
+
+          </td>
+
+          <td id='right'>
+
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </center>
+  )
+}
+
 }
 
 export default App;
